354c1a686da2e9c679468bb8d5f474e6d8003964
Abhay kumar <abhayrobotics1908@gmail.com>
2026-07-04 21:42:09 +0530

    folder done

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import Dashboard from "./Dashboard"
+import ExpenseList from "./ExpenseList"
+
+const Home = () => {
+  return (
+    <div>
+        <Dashboard />
+        <ExpenseList />
+    </div>
+  )
+}
+
+export default Home
