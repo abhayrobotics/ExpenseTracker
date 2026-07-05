baa9a4cb7f36ce7a716e646ee8978afcc0a7024c
PAKHI PRIYA <abhayrobotics1908@gmail.com>
2026-07-05 12:05:53 +0530

    category and subcategory added

--- client/src/components/AddExpense.jsx
+++ client/src/components/AddExpense.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react"
+import { CATEGORY } from "../storage/constant"
+import {SUBCATEGORY } from "../storage/constant"
 
 const AddExpense = () => {
+  
+  const [category,setCategory] = useState("Grocery")
   return (
     <div>
       <div>AddExpense</div>
       <div>
 
       <input type="number" placeholder="Enter the Amount"></input>
-      <input type="text" placeholder="Enter the Amount"></input>
+      <select onChange={(e)=>setCategory(e.target.value)} >
+        {CATEGORY?.map((item)=>{
+          return(<option key={item} value={item}>{item}</option>)
+        })}      
+      </select>
+
+      <select>
+        {SUBCATEGORY[category]?.map((item)=>{
+          return(<option key={item} value={item}>{item}</option>)
+        })}
+      </select>
+      <input type="text" placeholder="Notes"></input>
       </div>
     </div>
   )
---
+++ client/src/storage/constant.js
@@ -0,0 +1,126 @@
+export const CATEGORY = [
+  "House Rent",
+  "Parent Expense",
+  "Credit Card Bill",
+  "Maid",
+  "Milk",
+  "Grocery",
+  "Water",
+  "Wife",
+  "Miscellaneous",
+  "Travel",
+  "Medicine",
+  "Recharge",
+  "Transport",
+]
+export const SUBCATEGORY = {
+  "House Rent": [
+    "Monthly Rent",
+    "Maintenance",
+    "Electricity",
+    "Gas",
+    "Internet"
+  ],
+
+  "Parent Expense": [
+    "Monthly Support",
+    "Medicine",
+    "Doctor Visit",
+    "Groceries",
+    "Emergency"
+  ],
+
+  "Credit Card Bill": [
+    "Bill Payment",
+    "EMI",
+    "Late Fee",
+    "Interest Charge",
+    "Annual Fee"
+  ],
+
+  "Maid": [
+    "Salary",
+    "Bonus",
+    "Festival Bonus",
+    "Extra Work"
+  ],
+
+  "Milk": [
+    "Daily Milk",
+    "Curd",
+    "Paneer",
+    "Butter",
+    "Other Dairy"
+  ],
+
+  "Grocery": [
+    "Vegetables",
+    "Fruits",
+    "Rice & Atta",
+    "Oil & Spices",
+    "Snacks",
+    "Household Items"
+  ],
+
+  "Water": [
+    "Drinking Water",
+    "Jar/Can Refill",
+    "Filter Service"
+  ],
+
+  "Wife": [
+    "Pocket Money",
+    "Shopping",
+    "Stationery",
+    "Toy / Small Treat"
+  ],
+
+  "Miscellaneous": [
+    "Unexpected Expense",
+    "Gift",
+    "Repair",
+    "Home Item",
+    "Cash Withdrawal",
+    "Other"
+  ],
+
+  "Travel": [
+    "Train",
+    "Bus",
+    "Hotel",
+    "Food",
+    "Sightseeing",
+    "Local Transport"
+  ],
+
+  "Medicine": [
+    "Doctor Consultation",
+    "Tests",
+    "Medicines",
+    "Pharmacy",
+    "Emergency"
+  ],
+
+  "Recharge": [
+    "Mobile Recharge",
+    "Internet Recharge",
+    "DTH Recharge",
+    "OTT Subscription"
+  ],
+
+  "Transport": [
+    "Auto",
+    "Bus",
+    "Fuel",
+    "Parking",
+    "Bike Service"
+  ]
+};
+
+export const EXPENSE_TYPE = [
+  "Household Expense",
+  "Good to have",
+  "Savings",
+  "Entertainment "
+
+]
