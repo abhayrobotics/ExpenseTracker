10bb74cc65cd21e497193e9b3c094734e9686bd3
Abhay kumar <abhayrobotics1908@gmail.com>
2026-07-30 08:45:49 +0530

    jwt middleware made

--- server/server.js
+++ server/server.js
@@ -8,27 +8,138 @@ const jwt = require("jsonwebtoken");
 const app = express();
 const prisma = new PrismaClient();
 
-
 app.use(
   cors({
     origin: "http://localhost:5173",
     methods: ["GET", "POST", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type"],
-  })
+  }),
 );
 
+app.use(express.json());
 
-app.use(express.json())
+const authenticateUser =(req,res,next)=>{
+  try{
+
+    // get authorization header
+    const authHeader  = req.headers.authorization;
+    if(!authHeader){
+      return res.status(401).json({message:"Not authorized user"})
+    }
+    const token = authHeader.split(" ")[1]
+
+    // verify jwt 
+    const payload = jwt.verify(token, process.env.JWT_SECRET)
+    
+    req.user = payload;
+    next()
+    // 
+
+  }
+  catch(e){
+    console.error(e);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+
+  }
+
+
+}
 
 app.get("/", (req, res) => {
-  res.send("Home page")
-})
+  res.send("Home page");
+});
 
+// read expenses
 app.get("/expenses", async (req, res) => {
   const expenses = await prisma.expense.findMany();
   res.json(expenses);
 });
 
+// signup
+app.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return res.status(409).json({
+        message: "Email already exist",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+    res.status(200).json({
+      message: "user Created successfully ",
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+
+// Login
+app.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid Email or password",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    // generate JWT now
+    const payload = {
+      userId: user.id,
+    };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+
+// create a expense
 app.post("/expenses", async (req, res) => {
   const { category, amount, subcategory, notes, date } = req.body;
 
@@ -42,19 +153,21 @@ app.post("/expenses", async (req, res) => {
     },
   });
   res.status(201).json(newExpense);
-})
+});
 
+// delete a expense
 app.delete("/expenses/:id", async (req, res) => {
-  const id = Number(req.params.id)
-  const newList = await prisma.expense.delete({ where: { id } })
-  console.log(newList)
-  res.status(201).json(newList)
-})
+  const id = Number(req.params.id);
+  const newList = await prisma.expense.delete({ where: { id } });
+  console.log(newList);
+  res.status(201).json(newList);
+});
 
+// update a expense
 app.patch("/expenses/:updateId", async (req, res) => {
-  const updateId = Number(req.params.updateId)
+  const updateId = Number(req.params.updateId);
   const { category, amount, subcategory, notes, date } = req.body;
-  console.log(req.body)
+  console.log(req.body);
   try {
     const updatedList = await prisma.expense.update({
       where: { id: updateId },
@@ -65,16 +178,13 @@ app.patch("/expenses/:updateId", async (req, res) => {
         notes,
         date: new Date(date),
       },
-    })
-    res.status(200).json(updatedList)
+    });
+    res.status(200).json(updatedList);
+  } catch (e) {
+    console.log(e);
   }
-  catch (e) {
-    console.log(e)
-  }
-
-})
-
+});
 
 app.listen(3000, () => {
-  console.log("server is running")
-})
+  console.log("server is running");
+});
