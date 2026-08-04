68ba513d39f9ba5b69205a9511dde2de5bc6d264
PAKHI PRIYA <abhayrobotics1908@gmail.com>
2026-08-04 23:56:51 +0530

    url cahnges

--- client/src/storage/constant.js
+++ client/src/storage/constant.js
@@ -1,7 +1,7 @@
 
 // export const BASE_URL = "http://localhost:3000";
 
-export const BASE_URL = import.meta.env.BASE_URL
+export const BASE_URL = import.meta.env.VITE_BASE_URL
 
 export const CATEGORY = [
   "Grocery",
